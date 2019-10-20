6d9a72df0877c80f887580c95bc5e42413e44bfe
Reece Dunham <me@rdil.rocks>
2019-10-20 19:23:05 +0000

    refactor navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,62 +6,45 @@ import Typography from "@material-ui/core/Typography"
 import IconButton from "@material-ui/core/IconButton"
 import MenuIcon from "@material-ui/icons/Menu"
 
-class NavigationBar extends React.Component {
-    constructor(props) {
-        super(props)
-        this.showDrawer = false
-        this.classes = makeStyles(theme => ({
-            root: {
-                flexGrow: 1
-            },
-            menuButton: {
-                marginRight: theme.spacing(2)
-            },
-            title: {
-                flexGrow: 1
-            }
-        }))
-        this.state = {
-            showDrawer: false
+export default props => {
+    const classes = makeStyles(theme => ({
+        root: {
+            flexGrow: 1
+        },
+        menuButton: {
+            marginRight: theme.spacing(2)
+        },
+        title: {
+            flexGrow: 1
         }
-    }
+    }))
 
-    toggleDrawer() {
-        this.setState({
-            showDrawer: true
-        })
-    }
-
-    render() {
-        return (
-            <div className={this.classes.root}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <a
-                            href="https://github.com/RDIL/skyblock-minion-calc"
-                            target="_blank"
-                            style={{
-                                color: "inherit"
-                            }}
-                            rel="noopener noreferrer"
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <a
+                        href="https://github.com/RDIL/skyblock-minion-calc"
+                        target="_blank"
+                        style={{
+                            color: "inherit"
+                        }}
+                        rel="noopener noreferrer"
+                    >
+                        <IconButton
+                            edge="start"
+                            className={classes.menuButton}
+                            color="inherit"
+                            aria-label="menu"
                         >
-                            <IconButton
-                                edge="start"
-                                className={this.classes.menuButton}
-                                color="inherit"
-                                aria-label="menu"
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                        </a>
-                        <Typography variant="h6" className={this.classes.title}>
-                            Skyblock Minion Cost Calculator
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        )
-    }
+                            <MenuIcon />
+                        </IconButton>
+                    </a>
+                    <Typography variant="h6" className={classes.title}>
+                        Skyblock Minion Cost Calculator
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
 }
-
-export default NavigationBar
