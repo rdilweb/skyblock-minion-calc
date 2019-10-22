1fb930a53636e1780a9b2c98d8d859b4c2f517b5
Reece Dunham <me@rdil.rocks>
2019-10-22 21:24:01 +0000

    fixes, tweaks, etc (normal stuffs)

--- src/data/LogicHandler.js
+++ src/data/LogicHandler.js
@@ -1,48 +1,6 @@
 import Minions from "./Minions"
-import "../js-roman-numerals"
 import RomanNumeral from "js-roman-numerals"
-
-let woodMinions = {
-    I: 80,
-    II: 160,
-    III: 320,
-    IV: 512,
-    V: 1280,
-    VI: 2560,
-    VII: 5120,
-    VIII: 10240,
-    IX: 20480,
-    X: 40960,
-    XI: 81920
-}
-
-let combatCommon = {
-    I: 80,
-    II: 160,
-    III: 320,
-    IV: 512,
-    V: 1280,
-    VI: 2560,
-    VII: 5120,
-    VIII: 10240,
-    IX: 20480,
-    X: 40960,
-    XI: 81920
-}
-
-let farmCommon = {
-    I: 128,
-    II: 256,
-    III: 512,
-    IV: 1280,
-    V: 3840,
-    VI: 10240,
-    VII: 20480,
-    VIII: 40960,
-    IX: 81920,
-    X: 204800,
-    XI: 409600
-}
+import { woodMinions, combatCommon, farmCommon } from "./Store"
 
 export default (tier, minion) => {
     let e = new RomanNumeral(tier).toString()
